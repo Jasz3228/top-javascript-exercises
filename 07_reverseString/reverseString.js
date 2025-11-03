e7548730a14f6d749bee4fc7fc5c892bb027c2b7
Jordan Stewart <{ID}+{username}@users.noreply.github.com>
2025-11-03 14:55:33 -0500

    Completed exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let stringArr = str.split("");
+  
+  let reversedArr = stringArr.reverse();
 
-};
+  let reversedStr = reversedArr.join("");
+  
+  return reversedStr;
+
+ 
+}
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
