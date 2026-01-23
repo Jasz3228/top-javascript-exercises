016a785a3d95dc381855ac4d0ac998210ca8faef
Jordan Stewart <149850673+Jasz3228@users.noreply.github.com>
2026-01-22 21:27:17 -0500

    Completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = Number(n);
 
+  if (n < 0) {
+      return "OOPS";
+  } else if (n === 0) {
+      return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  let indexOne = 1;
+  let indexTwo = 1;
+  
+  for (let i = 3; i <= n; i++) {
+    let sum = indexOne + indexTwo;
+    indexOne = indexTwo;
+    indexTwo = sum;
+  }
+  return indexTwo;
 };
 
 // Do not edit below this line
