adf3a362aacb1796f356036d52db739339bef8ba
Jordan Stewart <149850673+Jasz3228@users.noreply.github.com>
2026-01-27 12:52:17 -0500

    Completed exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.sort((a, b) => {
+    // Calculate a's age (handle undefined yearOfDeath)
+    const ageA = a.yearOfDeath ? (a.yearOfDeath - a.yearOfBirth) :
+      (new Date().getFullYear() - a.yearOfBirth);
 
-};
+    // Calculate b's age
+    const ageB = b.yearOfDeath ? (b.yearOfDeath - b.yearOfBirth) :
+      (new Date().getFullYear() - b.yearOfBirth);
+
+    return ageB - ageA;
+  })[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
